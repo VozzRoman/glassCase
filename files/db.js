e05a9bfcad8cefe93acbf555f62e2eb09966d961
VozzRoman <looper0500@ukr.net>
2023-12-10 10:13:50 +0200

    adding

---
+++ files/db.js
@@ -0,0 +1,41 @@
+export const data = [
+	{
+		title: "Nike",
+		code:'N050102',
+		description: "Високі якісні <strong>зимові</strong> челсі на хутрі",
+		colors: ["red", "white", "black"],
+		material: ["шкipa"],
+		size: ["36", "37", "38", "39", "40", "41"],
+		price: "2600",
+		salePrice: "2300",
+		images: ["img/nike_1.jpg", "img/nike_2.jpg", "img/nike_3.jpg", "img/nike_4.jpg", "img/nike_5.jpg"],
+		navImages:["img/nike_1-slider.jpg", "img/nike_2-slider.jpg", "img/nike_3-slider.jpg", "img/nike_4-slider.jpg", "img/nike_5-slider.jpg"],
+		id: "1",
+	},
+	{
+		title: "Adidas Campus Black Delusion ",
+		code:'A113001',
+		description: "<strong>Зимові кросівки</strong> унісекс.Стильний дизайн з широкими шнурівками.",
+		colors: ["black"],
+		material: ["шкipa"],
+		size: ["41", "42", "43", "44", "45", "46"],
+		price: "2750",
+		salePrice: "2500",
+		images: ["img/adidas_1.jpg", "img/adidas_2.jpg", "img/adidas_3.jpg", "img/adidas_4.jpg", "img/adidas_5.jpg"],
+		navImages:["img/adidas_1-slider.jpg", "img/adidas_2-slider.jpg", "img/adidas_3-slider.jpg", "img/adidas_4-slider.jpg", "img/adidas_5-slider.jpg"],
+		id: "2"
+	},
+	{
+		title: "Sneakers Ayron Beige",
+		code:'B520301',
+		description: "<strong>Зимові високі  кросівки</strong> у 3-ох базових кольорах. Йдуть на шнурівці, легка та практична модель",
+		colors: ["white"],
+		material: ["шкipa"],
+		size: ["36", "37", "38", "39", "40", "41"],
+		price: "1870",
+		salePrice: "1600",
+		images: ["img/Sneakers_1.jpg", "img/Sneakers_2.jpg", "img/Sneakers_3.jpg", "img/Sneakers_4.jpg", "img/Sneakers_5.jpg"],
+		navImages:["img/Sneakers_1-slider.jpg", "img/Sneakers_2-slider.jpg", "img/Sneakers_3-slider.jpg", "img/Sneakers_4-slider.jpg", "img/Sneakers_5-slider.jpg"],
+		id: "3"
+	}
+]
